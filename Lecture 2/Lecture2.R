59d39073854ad009e9abe0d586fdb1bfab1417a1
catodavies <75034099+catodavies@users.noreply.github.com>
2022-10-13 14:20:59 +0100

    Update Lecture2.R

--- Lecture 2/Lecture2.R
+++ Lecture 2/Lecture2.R
@@ -1,4 +1,14 @@
+library(tidyverse)
 
+my_function <- function(number1, number2){
+  a+b
+}
+my_function()
 
+iqr <- function(v){
+  upper <- as.numeric(quantile(v,0.75))
+  lower <- as.numeric(quantile(v,0.25))
+  return(upper  - lower)
+}
 
-
+iqr(mpg$hwy)
