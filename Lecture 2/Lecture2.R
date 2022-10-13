f4a0131f1363086553cdd8547393a8591209a628
catodavies <75034099+catodavies@users.noreply.github.com>
2022-10-13 14:46:26 +0100

    Update Lecture2.R

--- Lecture 2/Lecture2.R
+++ Lecture 2/Lecture2.R
@@ -20,3 +20,26 @@ if(13 %% 2 != 0 ){
   print("even")
   
 }
+
+cor_mpg_dipl <- function(x){
+  
+  df <- mpg %>%
+    filter(class == x)
+  
+  answer <- cor(df$cty,df$displ)
+  
+  return(answer)
+}
+
+cor_mpg_dipl("compact")
+
+
+
+if(13.7 %% 1 != 0){
+  print("Not an Integer")
+} else if(13 %% 2 != 0 ){
+  print("odd")
+} else{
+  print("even")
+} 
+
