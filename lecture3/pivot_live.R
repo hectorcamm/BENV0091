44d913ebbfd248c254ca6487739f34ac1cedbf69
hectorcamm <75304118+hectorcamm@users.noreply.github.com>
2022-10-20 07:55:34 +0100

    PUSHES LIVE

---
+++ lecture3/pivot_live.R
@@ -0,0 +1,10 @@
+# Read in the librarys
+
+library(tidyverse)
+
+# Read in the data
+energy <- read_csv('data/energy_consumption_by_sector_fuel.csv')
+
+### Task: use pivot_longer() to make the energy data tidy
+pivot_longer()
+gather()
