bac8eb3b8acdf4736b4edd58043f2aa3bc57ef0a
hectorcamm <75304118+hectorcamm@users.noreply.github.com>
2022-10-20 08:09:17 +0100

    push live

--- lecture3/pivot_live.R
+++ lecture3/pivot_live.R
@@ -9,15 +9,22 @@ energy <- read_csv('data/energy_consumption_by_sector_fuel.csv')
 pivot_longer()  /  pivot_wider()
 gather()  /   spread()
 
+## Option 1 
 longer <- energy %>% 
-  gather(key="fuel",value="consumption",Solid_Fuel:Electricity) %>% system.time()
+  gather(key="fuel",value="consumption",Solid_Fuel:Electricity) 
 
+# Option 2
 longer <- energy %>% 
   pivot_longer(Solid_Fuel: Electricity,
                names_to = 'fuel',
-               values_to = 'consumption') %>% system.time()
+               values_to = 'consumption')
 
-### Task: create the plot on the right with ggplot2 and geom_line()
+# To decide which option to use consider system.time() 
+# Or simply how well the documentation is written
+??tidyr::pivot_longer
+??tidyr::gather 
+
+### Task Hint (Solution to follow): create the plot on the right with ggplot2 and geom_line()
 `data_to_use` %>% 
   ggplot() + 
   geom_line(aes(x = `variable_1`, 
